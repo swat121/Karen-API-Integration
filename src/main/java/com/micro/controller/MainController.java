e0e39c7530919432e36a1d30de042c2602aa8235
Laptop <stark2542@gmail.com>
2022-07-06 20:54:59 +0300

    Refactoring: removing unnecessary methods

--- src/main/java/com/micro/controller/MainController.java
+++ src/main/java/com/micro/controller/MainController.java
@@ -12,43 +12,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.ConnectException;
+import java.rmi.ConnectIOException;
+
 
 @RestController
 @RequiredArgsConstructor
 public class MainController {
 
+    private String resourceUrl = "http://192.168.0.100:80/";
     private final RestTemplate restTemplate;
     @SneakyThrows
-    @GetMapping("/garry/temperature")
-    public String temperature(){
-        String fooResourceUrl
-                = "http://192.168.0.100:80/temperature";
-        ResponseEntity<GarryResponse> response
-                = restTemplate.getForEntity(fooResourceUrl, GarryResponse.class);
-        return response.getBody().getTemperature();
-    }
-    @GetMapping("/garry/backlightOn")
-    public String backlightOn(){
-        String fooResourceUrl
-                = "http://192.168.0.100:80//setting?backlight=on";
-        ResponseEntity<GarryResponse> response
-                = restTemplate.getForEntity(fooResourceUrl, GarryResponse.class);
-        return response.getBody().getBacklight();
-    }
-    @GetMapping("/garry/backlightOff")
-    public String backlightOff(){
-        String fooResourceUrl
-                = "http://192.168.0.100:80//setting?backlight=off";
-        ResponseEntity<GarryResponse> response
-                = restTemplate.getForEntity(fooResourceUrl, GarryResponse.class);
-        return response.getBody().getBacklight();
+    @GetMapping("/garry/{key}")
+    public String data(@PathVariable(value="key") String key){
+        try {
+        ResponseEntity<GarryResponse> response;
+            switch (key) {
+                case "temperature":
+                    response = restTemplate.getForEntity(resourceUrl + key, GarryResponse.class);
+                    return response.getBody().getTemperature();
+                case "backlightOn":
+                    response = restTemplate.getForEntity(resourceUrl + "setting?backlight=on", GarryResponse.class);
+                    return response.getBody().getBacklight();
+                case "backlightOff":
+                    response = restTemplate.getForEntity(resourceUrl + "setting?backlight=off", GarryResponse.class);
+                    return response.getBody().getBacklight();
+                default:
+                    return "Такое еще не придумал";
+            }
+        } catch (Exception e){
+            return "Проверьте подключение ESP к сети";
+        }
     }
     @GetMapping("/garry/message/{text}")
     public String message(@PathVariable(value = "text") String text){
-        String fooResourceUrl
-                = "http://192.168.0.100:80//message?text=";
         ResponseEntity<GarryResponse> response
-                = restTemplate.getForEntity(fooResourceUrl+text, GarryResponse.class);
+                = restTemplate.getForEntity(resourceUrl+"message?text="+text, GarryResponse.class);
         return response.getBody().getMessage();
     }
 }
