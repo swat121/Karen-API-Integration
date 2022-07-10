04ac2453ec30cc9c755653593c151192703a6afe
Laptop <stark2542@gmail.com>
2022-07-10 13:16:41 +0300

    Fix: error

--- src/main/java/com/micro/KarenApplication.java
+++ src/main/java/com/micro/KarenApplication.java
@@ -1,12 +1,13 @@
 package com.micro;
 
+import com.micro.config.UrlConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 
 @SpringBootApplication
-@EnableConfigurationProperties
+@EnableConfigurationProperties(UrlConfig.class)
 public class KarenApplication {
 
 	public static void main(String[] args) {
