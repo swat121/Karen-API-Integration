a0fb04aa881f298911253ab4eefa53c373f78077
swat121 <stark2542@gmail.com>
2022-11-13 23:37:31 +0300

    Add: dicrovery service and load balanced

--- src/main/java/com/micro/KarenApplication.java
+++ src/main/java/com/micro/KarenApplication.java
@@ -4,8 +4,9 @@ import com.micro.config.UrlConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-
+@EnableEurekaClient
 @SpringBootApplication
 @EnableConfigurationProperties(UrlConfig.class)
 public class KarenApplication {
