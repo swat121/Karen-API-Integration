aa47f5364a63fa7b7fb0ee9e106b77958f42fd73
swat121 <stark2542@gmail.com>
2023-02-16 22:17:04 +0200

    Add: LOGGER

--- src/main/java/com/micro/controller/BotController.java
+++ src/main/java/com/micro/controller/BotController.java
@@ -3,6 +3,8 @@ package com.micro.controller;
 
 import com.micro.service.BotService;
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +14,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class BotController {
 
     private BotService botService;
+    private static final Logger LOG = LogManager.getRootLogger();
 
     @GetMapping("/bot/{message}")
     public String sendMessage(@PathVariable(value = "message") String message){
+        LOG.info("======================== BotController: GetMapping - " + message + " ========================");
         return botService.sendMessage(message);
     }
 }
--- src/main/java/com/micro/service/MicroControllerService.java
+++ src/main/java/com/micro/service/MicroControllerService.java
@@ -4,6 +4,8 @@ import com.micro.config.UrlConfig;
 import com.micro.exception.ApiRequestException;
 import com.micro.model.DataResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,25 +13,32 @@ import org.springframework.stereotype.Service;
 public class MicroControllerService {
     private final UrlConfig urlConfig;
     private final ConnectionService connectionService;
+    private static final Logger LOG = LogManager.getRootLogger();
 
     public String request(String name, String key) {
+        LOG.info("======================== Micro controller service: request - " + name + key + " ========================");
         switch (key) {
             case "help":
+                LOG.info("=========== Micro controller service: get " + key + " response from service - " + name + key + " ===========");
                 DataResponse dataResponse = connectionService.getResponseFromService(urlConfig.getResource().get(name), key, DataResponse.class);
                 return dataResponse.getName() + ": " + dataResponse.getHelp();
             case "status":
+                LOG.info("=========== Micro controller service: get " + key + " response from service - " + name + key + " ===========");
                 return connectionService.getResponseFromService(urlConfig.getResource().get(name), key, String.class);
             default:
+                LOG.info("=========== Micro controller service: get " + key + " command not found ===========");
                 throw new ApiRequestException(String.format("Command not found: [%s]", key));
         }
     }
 
     public String message(String name, String key, String text) {
+        LOG.info("======================== Micro controller service: message " + key + " response from service - " + name + key + " ========================");
         DataResponse dataResponse = connectionService.getResponseFromService(urlConfig.getResource().get(name), key + "?text=" + text, DataResponse.class);
         return dataResponse.getMessage();
     }
 
     public String setting(String name, String key) {
+        LOG.info("======================== Micro controller service: setting - " + name + key + " ========================");
         DataResponse dataResponse = connectionService.getResponseFromService(urlConfig.getResource().get(name), key, DataResponse.class);
         switch (key) {
             case "relay1":
@@ -46,6 +55,7 @@ public class MicroControllerService {
     }
 
     public String sensor(String name, String key) {
+        LOG.info("======================== Micro controller service: sensor - " + name + key + " ========================");
         DataResponse dataResponse = connectionService.getResponseFromService(urlConfig.getResource().get(name), key, DataResponse.class);
         switch (key) {
             case "temperature":
